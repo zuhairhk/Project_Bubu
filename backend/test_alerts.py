7fd990bd731b476d10fd138be0f8e714377bcc00
Zuhair Khan <zuhairhk@gmail.com>
2025-11-07 14:52:03 -0500

    Testing YRT Protocol Buffer file

---
+++ backend/test_alerts.py
@@ -0,0 +1,14 @@
+from google.transit import gtfs_realtime_pb2
+
+# Load binary .pb file
+with open("alerts.pb", "rb") as f:
+    data = f.read()
+
+feed = gtfs_realtime_pb2.FeedMessage()
+feed.ParseFromString(data)
+
+for entity in feed.entity:
+    if entity.HasField("alert"):
+        header = entity.alert.header_text.translation[0].text
+        desc = entity.alert.description_text.translation[0].text if entity.alert.description_text.translation else ""
+        print(f"⚠️ {header}\n{desc}\n")
